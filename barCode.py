457d8626a95843d71d7ee3717707c83da891feb7
hyunseokjoo <joohyunseok0313@gmail.com>
2021-12-29 16:26:48 +0900

    주석 설정

--- barCode.py
+++ barCode.py
@@ -3,13 +3,20 @@ import pyzbar.pyzbar as pyzbar
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
+# 바코드 인식 및 테두리 설정
 def read_frame(frame):
     try:
+        # 바코드 정보 decoding
         barcodes = pyzbar.decode(frame)
+        # 바코드 정보가 여러개 이기 때문에 하나씩 해석
         for barcode in barcodes:
+            # 바코드 rect정보
             x, y, w, h = barcode.rect
+            # 바코드 데이터 디코딩
             barcode_info = barcode.data.decode('utf-8')
+            # 인식한 바코드 사각형 표시
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            # 인식한 바코드 사각형 위에 글자 삽입
             cv2.putText(frame, barcode_info, (x , y - 20), font, 0.5, (0, 0, 255), 1)
 
         return frame
@@ -19,13 +26,18 @@ def read_frame(frame):
 
 def main():
     try:
+        # 동영상 불러오기
         cap = cv2.VideoCapture("barcode.mp4")
+
+        # 동영상 연결 되었는지 확인 및 영상 재생
         while cap.isOpened():
+            # 실행 내역 및 프레임 가져오기
             ret, frame = cap.read()
-            print(type(frame))
-            print(type(""))
+            # 실행 내역이 true이면 프레임 출력
             if ret:
+                # 바코드 인식
                 frame = read_frame(frame)
+                # 프레임 출력
                 cv2.imshow("barcode reader", frame)
                 if cv2.waitKey(1) == 27:
                     break
@@ -33,8 +45,6 @@ def main():
                 print("예외")
                 break
 
-        cap.release()
-        cv2.destroyAllWindows()
     except Exception as e:
         print(e)
     finally:
